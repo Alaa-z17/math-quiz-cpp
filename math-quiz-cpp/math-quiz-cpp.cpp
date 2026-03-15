2e9f09dd409766cd1a5124639f2933baa25df991
Alaa Alkatshah <alaaalkatshah@gmail.com>
2026-03-15 22:59:45 +0300

    feat: add RandomNumber and SimpleCalculator helpers

--- math-quiz-cpp/math-quiz-cpp.cpp
+++ math-quiz-cpp/math-quiz-cpp.cpp
@@ -5,6 +5,29 @@ enum enQuestionsLevel { EasyLevel = 1, MedLevel = 2, HardLevel = 3, Mix = 4 };
 
 enum enOperationType { Add = 1, Sub = 2, Mult = 3, Div = 4, MixOp = 5 };
 
+int RandomNumber(int From, int To)
+{
+    int randNum = rand() % (To - From + 1) + From;
+    return randNum;
+}
+
+int SimpleCalculator(int Number1, int Number2, enOperationType OpType)
+{
+    switch (OpType)
+    {
+    case enOperationType::Add:
+        return Number1 + Number2;
+    case enOperationType::Sub:
+        return Number1 - Number2;
+    case enOperationType::Mult:
+        return Number1 * Number2;
+    case enOperationType::Div:
+        return Number1 / Number2;
+    default:
+        return Number1 + Number2;
+    }
+}
+
 struct stQuestion
 {
     int Number1 = 0;
